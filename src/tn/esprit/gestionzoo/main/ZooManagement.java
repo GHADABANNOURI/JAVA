86f18faf59305debbb9fb68779fda0e511b4eb96
ghada <ghada.bannouri@esprit.tn>
2023-12-09 00:31:00 +0900

    prosit6

--- src/tn/esprit/gestionzoo/main/ZooManagement.java
+++ src/tn/esprit/gestionzoo/main/ZooManagement.java
@@ -1,66 +1,90 @@
 package tn.esprit.gestionzoo.main;
-
 import tn.esprit.gestionzoo.entities.*;
+import java.util.Scanner;
 
 public class ZooManagement {
+    public static void main(String[] args){
+        //-------------------------------- Prosit 1 -------------------------------------//
 
-    public static void main(String[] args) {
-        Animal lion = new Animal();
-        lion.setName("Simba");
-        lion.setAge(8);
-        lion.setFamily("Cats");
-        lion.setMammal(true);
+        /*int nbrCages = 20;
+        String zooName = "my zoo";
 
-        Zoo myZoo = new Zoo("Wildlife Park", "Ariana");
-        Zoo notMyZoo = new Zoo("WaterPark", "Siliana");
+        System.out.println(zooName + " comporte " + nbrCages + " cages");
 
+        Scanner scn = new Scanner(System.in);
+        Scanner scn1 = new Scanner(System.in);
 
-        Animal dog = new Animal("Canine", "Snoopy", 2, true);
+        System.out.println("Entrer le nombre de cages: ");
+        nbrCages = scn.nextInt();
+        System.out.println("Enterer le nom du zoo: ");
+        zooName = scn1.nextLine();
 
-
-        System.out.println(myZoo.addAnimal(lion));
-        System.out.println(myZoo.addAnimal(dog));
-
-        myZoo.displayAnimals();
-
-        System.out.println(myZoo.searchAnimal(dog));
-        Animal dog2 = new Animal("Canine", "lll", 2, true);
-        System.out.println(myZoo.searchAnimal(dog2));
-
-//           System.out.println(myZoo.removeAnimal(dog));
-        myZoo.displayAnimals();
+        System.out.println(zooName + " comporte " + nbrCages + " cages");*/
 
 
-        System.out.println(myZoo);
+        //-------------------------------- Prosit 2 + 3-------------------------------------//
 
-        myZoo.addAnimal(lion);
-        myZoo.addAnimal(dog);
-        myZoo.addAnimal(dog2);
-        myZoo.displayAnimals();
+        /*Zoo myZoo = new Zoo("myZoo", "Tunis");
+        Zoo myZoo2 = new Zoo("myZoo2", "Ben Gerdan");
+        Animal a1 = new Animal("Otariidae", "Lion", 10, true);
+        Animal a2 = new Animal("Canidae", "Wolf", 8, false);
+        Animal a3 = new Animal("Canidae", "Wolf", 20, false);
+        Animal a4 = new Animal("Otariidae", "Lion", 10, true); //identique de a1 pour vérifier addAnimal() function and searchAnimal()
 
+        //myZoo.displayZoo();
+        //System.out.println(myZoo);
 
-        myZoo.setName("Belvedere Park");
-        Zoo notMyZoo1 = Zoo.comparerZoo(myZoo, notMyZoo);
-        System.out.println(notMyZoo1);
+        myZoo.addAnimal(a1);
+        myZoo.addAnimal(a2);
+        myZoo.addAnimal(a3);
+        System.out.println(myZoo.addAnimal(a4));
+        myZoo.displayZoo();
 
-        System.out.println("-------------------------------------------------------");
-        Aquatic aquatic = new Aquatic("Fish", "Sardine", 2, true, "Sea");
-        Terrestrial terrestrial = new Terrestrial("Panda", "Narla", 4, true, 2);
-        Dolphin dolphin = new Dolphin("Delphinidae", "Flipper", 5, true, "Ocean", 14.5f);
-        Penguin penguin = new Penguin("Spheniscidae", "Skipper", 3, true, "Ocean", 25.3f);
+        myZoo.removeAnimal(a1);
+        myZoo.displayZoo();
+        System.out.println(myZoo.isZooFull()); //False because myZoo ain't full.
+        System.out.println(Zoo.comparerZoo(myZoo, myZoo2));*/
 
+        //-------------------------------- Prosit 4 -------------------------------------//
 
-        System.out.println(aquatic);
-        System.out.println(terrestrial);
-        System.out.println(dolphin);
-        System.out.println(penguin);
+        /*Zoo myZoo = new Zoo("", "Tunis"); //Just to check the empty name
+        Animal a1 = new Animal("Canidae", "Wolf", -1, true); //Checking the negative Age*/
 
+        //-------------------------------- Prosit 5 -------------------------------------//
 
+        /*Aquatic a1 = new Aquatic();
+        Terrestrial t1 = new Terrestrial();
+        Dolphin d1 = new Dolphin();
+        Penguin p1 = new Penguin();
 
-        aquatic.swim();
-        dolphin.swim();
-        penguin.swim();
+        System.out.println(a1.toString()); //
+        System.out.println(t1.toString()); //
+                                           // => All of these variables have empty attributes due to Default Constructor.
+        System.out.println(d1.toString()); //
+        System.out.println(p1.toString()); //
 
+        a1.swim();
+        d1.swim(); //swim() function a ete redefini ici
+        p1.swim(); //swim() function n'a pas ete redefini ici*/
+
+        //-------------------------------- Prosit 6 -------------------------------------//
+
+        Zoo myZoo = new Zoo("myZoo", "Tunis");
+        Aquatic a1 = new Aquatic("Mammal", "Dog", 5, true, "test");
+        Aquatic d1 = new Dolphin("Mammal", "Cat", 10, true, "test", 14.5f);
+        Aquatic p1 = new Penguin("Mammal", "Penguin", 5, false, "test1", 10.4f);
+        Aquatic p2 = new Penguin("Mammal", "Penguin", 5, false, "test2", 5.4f);
+        Aquatic p3 = new Penguin("Mammal", "Penguin", 5, false, "test1", 19f);
+
+        myZoo.addAquaticAnimal(a1);
+        myZoo.addAquaticAnimal(d1);
+        myZoo.addAquaticAnimal(p1);
+        myZoo.addAquaticAnimal(p2);
+        myZoo.addAquaticAnimal(p3);
+        myZoo.instruction27();
+        System.out.println(myZoo.maxPenguinSwimmingDepth()); //19.0 is the result
+        myZoo.displayNumberOfAquaticsByType(); // Number of Dolphins: 1 <br> Number of Penguins: 3
+        System.out.println(p1.equals(p2)); //false not the same habitat
+        System.out.println(p1.equals(p3)); //true
     }
-
 }
