1a88ec20774a673b6124f4a323c3e3abfc8d3f92
ghada <ghada.bannouri@esprit.tn>
2023-12-09 18:16:02 +0900

    prosit10

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,37 @@ import java.util.TreeSet;
 public class Main {
     public static void main(String[] args) {
         /*SocieteArrayList S = new SocieteArrayList();
-        Employee e1 = new Employee(1, "Jendoubi", "Hafedh", "J24", 16);
-        Employee e2 = new Employee(2, "Jendoubi", "Majdi", "G40", 13);
-        Employee e3 = new Employee(3, "Klai", "Rayen", "A23", 15);
-        Employee e4 = new Employee(4, "Mhamdi", "Ahlem", "A23", 18);
+        Employee e1 = new Employee(1, "ghada", "bannouri", "J24", 16);
+        Employee e2 = new Employee(2, "aya", "bannouri", "G40", 13);
+        Employee e3 = new Employee(3, "abdelghani", "bannouri", "A23", 15);
+        Employee e4 = new Employee(4, "khalil", "bannouri", "A23", 18);
 
-        S.ajouterEmploye(e4); //Add works
+        S.ajouterEmploye(e4); 
         S.ajouterEmploye(e2);
         S.ajouterEmploye(e1);
         S.ajouterEmploye(e3);
-        System.out.println(S.rechercherEmploye(e1)); //True, it works
-        S.displayEmploye(); //Display works
-        System.out.println("\n"); //Display purposes
-        S.supprimerEmploye(e1); //Delete works
+        System.out.println(S.rechercherEmploye(e1)); 
+        S.displayEmploye(); 
+        System.out.println("\n"); 
+        S.supprimerEmploye(e1); 
         S.displayEmploye();
-        System.out.println(S.rechercherEmploye("Andolsi")); //False, it works
-        System.out.println("\n"); //Display purposes
+        System.out.println(S.rechercherEmploye("banour")); 
+        System.out.println("\n"); 
         S.trierEmployeParId(); //Tri ID works
         S.displayEmploye();
-        System.out.println("\n"); //Display purposes
-        S.trierEmployeParNomDepartementEtGrade(); //Tri NomDepartement then Grade works
+        System.out.println("\n"); 
+        S.trierEmployeParNomDepartementEtGrade(); 
         S.displayEmploye();*/
 
         /* ---------------------------------------- Prosit 10 ------------------------------------------------------ */
 
         DepartementHashSet D = new DepartementHashSet();
 
-        D.ajouterDepartement(new Departement(3, "J24", 15)); //Add Works
+        D.ajouterDepartement(new Departement(3, "J24", 15)); 
         D.ajouterDepartement(new Departement(2, "J23", 15));
         D.ajouterDepartement(new Departement(1, "J22", 15));
-        D.displayDepartement(); //It works
+        D.displayDepartement(); 
         System.out.println(D.rechercherDepartement("J24"));
-        System.out.println(D.trierDepartementById()); //It works fine
+        System.out.println(D.trierDepartementById()); 
     }
 }
