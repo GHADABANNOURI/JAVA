74da6ed5b085aadb3fd1491c1fb7c166ed3ca5e1
ghada <ghada.bannouri@esprit.tn>
2023-12-09 18:25:01 +0900

    prosit9

--- src/SocieteArrayList.java
+++ src/SocieteArrayList.java
@@ -3,66 +3,69 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class SocieteArrayList implements IGestion<Employee>{
-    List<Employee> Employees = new ArrayList<Employee>();
+public class SocieteArrayList implements IGestion<Employe> {
 
-    public SocieteArrayList(){}
+    List<Employe> employes;
+
+    public SocieteArrayList() {
+        employes = new ArrayList<>();
+    }
 
     @Override
-    public void ajouterEmploye(Employee employee) {
-        Employees.add(employee);
+    public void ajouterEmploye(Employe employe) {
+        employes.add(employe);
     }
 
     @Override
     public boolean rechercherEmploye(String nom) {
-        for(Employee e : Employees){
-            if(e.getNom() == nom){
+        // method 1
+        for (int i = 0; i < employes.size(); i++) {
+            if (employes.get(i).getNom().equals(nom))
                 return true;
-            }
         }
+        // method 2
+//        for (Employe employe : employes) {
+//            if (employe.getNom().equals(nom))
+//                return true;
+//        }
         return false;
     }
 
     @Override
-    public boolean rechercherEmploye(Employee employee) {
-        for(Employee e : Employees){
-            if(e == employee)
-                return true;
-        }
-        return false;
+    public boolean rechercherEmploye(Employe employe) {
+        return employes.contains(employe);
     }
 
     @Override
-    public void supprimerEmploye(Employee employee) {
-        Employees.remove(employee);
+    public void supprimerEmploye(Employe employe) {
+        employes.remove(employe);
     }
 
     @Override
     public void displayEmploye() {
-        System.out.println(Employees);
+        System.out.println(employes);
     }
 
     @Override
     public void trierEmployeParId() {
-        Collections.sort(Employees);
+        Collections.sort(employes);
     }
 
     @Override
     public void trierEmployeParNomDepartementEtGrade() {
-        Comparator<Employee> NomDepartementCompartor = new Comparator<Employee>() {
+        Comparator<Employe> departementComparator = new Comparator<Employe>() {
             @Override
-            public int compare(Employee o1, Employee o2) {
-                return o1.getNomDepartement().compareTo(o2.getNomDepartement());
+            public int compare(Employe o1, Employe o2) {
+                return o1.getDepartement().compareTo(o2.getDepartement());
             }
         };
-
-        Comparator<Employee> GradeCompartor = new Comparator<Employee>() {
+        Comparator<Employe> gradeComparator = new Comparator<Employe>() {
             @Override
-            public int compare(Employee o1, Employee o2) {
+            public int compare(Employe o1, Employe o2) {
                 return o1.getGrade() - o2.getGrade();
             }
         };
+        Collections.sort(employes, departementComparator.thenComparing(gradeComparator));
 
-        Collections.sort(Employees, NomDepartementCompartor.thenComparing(GradeCompartor));
     }
 }
